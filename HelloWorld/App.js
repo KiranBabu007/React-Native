517668f057917c24fc53d1afc7401aa908a7f639
Kiran Babu <kiranbabu3355@gmail.com>
2024-02-19 21:28:17 +0530

    Refactor App.js to use a plum background color and add a button

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Button
+        title="Press"
+        onPress={() => alert("Button clicked aane")}
+        color="midnightblue"
+        
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
