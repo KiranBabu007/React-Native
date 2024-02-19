834150cbbbb12ba72ac0942c5640d122c7965efd
Kiran Babu <kiranbabu3355@gmail.com>
2024-02-20 01:45:35 +0530

    Add modal to display a pop-up window when button is pressed

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,13 +1,28 @@
-import { View, Button } from "react-native";
+import { View, Button, Modal } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
       <Button
         title="Press"
-        onPress={() => alert("Button clicked")}
+        onPress={() => (isVisible ? setIsVisible(false) : setIsVisible(true))}
         color="midnightblue"
       />
+
+      <Modal
+        visible={isVisible}
+        style={{ flex: 1, backgroundColor: "plum", padding: 60 }}
+        animation="slide"
+      >
+        <Button
+          title="Back"
+          onPress={() => (isVisible ? setIsVisible(false) : setIsVisible(true))}
+          color="palevioletred"
+        />
+      </Modal>
     </View>
   );
 }
