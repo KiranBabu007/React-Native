ac1f5c23c9e0ef898ad0056e36093f6c55201ca5
Kiran Babu <kiranbabu3355@gmail.com>
2024-02-20 21:33:06 +0530

    Add getTypeDetails function to PokemonCard

--- Pokemon/Pokemon/components/PokemonCard.js
+++ Pokemon/Pokemon/components/PokemonCard.js
@@ -1,6 +1,19 @@
 import { View, Text, StyleSheet, Platform, Image } from "react-native";
 
-
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+};
 
 export default function PokemonCard({
   name,
