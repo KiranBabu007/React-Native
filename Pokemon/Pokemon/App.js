2a60232c25351c2b85e31ff350faa53df1632bac
Kiran Babu <kiranbabu3355@gmail.com>
2024-02-20 20:53:13 +0530

    Refactor App.js and PokemonCard.js

--- Pokemon/Pokemon/App.js
+++ Pokemon/Pokemon/App.js
@@ -1,18 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, Platform } from "react-native";
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
+
+  
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <SafeAreaView style={styles.container}>
+      <PokemonCard />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-   
+    backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS === "android" ? 15 : 0,
   },
 });
